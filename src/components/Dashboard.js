7d417cbd16e84714e695dd203590427c1b988b68
Milton Vandol Goh <100271526+Vandal19@users.noreply.github.com>
2022-08-16 00:31:21 -0700

    Build the panel to be use to render array of Panel components

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,10 +2,34 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 import Loading from "./Loading";
+import Panel from "./Panel"
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
   state = {
-    loading: true
+    loading: false
   };
 
   render() {
@@ -14,7 +38,17 @@ class Dashboard extends Component {
     if(this.state.loading) {
       return <Loading />;
     }
-    return <main className={dashboardClasses} />;
+
+    const dataPanel = data.map((panel) => {
+      return
+      <Panel
+        key={panel.id}
+        id={panel.id}
+        label={panel.label}
+        value={panel.value}
+      />
+    });
+    return <main className={dashboardClasses}>{dataPanel}</main>;
   }
 }
 
